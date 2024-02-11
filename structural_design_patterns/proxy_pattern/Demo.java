d3513216f7cfa9c02a1e6123a45c36977721a0f5
NikhilChib10 <nikchib10@gmail.com>
2024-02-11 13:00:41 +0530

    Proxy pattern

---
+++ structural_design_patterns/proxy_pattern/Demo.java
@@ -0,0 +1,35 @@
+package structural_design_patterns.proxy_pattern;
+
+import structural_design_patterns.proxy_pattern.downloader.YouTubeDownloader;
+import structural_design_patterns.proxy_pattern.some_cool_media_library.ThirdPartyYouTubeClass;
+import structural_design_patterns.proxy_pattern.some_cool_media_library.YouTubeCacheProxy;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeClass());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(YouTubeDownloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
